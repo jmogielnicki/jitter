c5c1c43ed6cf99649a582be289426d483642513b
John <jmogielnicki@gmail.com>
2015-09-07 21:23:15 -0700

    Fixed some bugs, repositioned buttons & sliders, added size parameter/slider

--- sketch.js
+++ sketch.js
@@ -12,7 +12,9 @@ var modeCurrent = 0;
 var firstTimeSwitch = true;
 var activeAnchorX = 200;
 var activeAnchorY = 200;
-var bugColor = [255,255,255,255] 
+var bugColor = [255,255,255,255]
+var bugOutline = [0,0,0,0]
+var bugSize = 1;
 
 
 
@@ -23,28 +25,36 @@ function setup() {
   canvas.mouseOut( function() { onCanvas = false; });
   // Create object
   bug = new Jitter();
+  noCursor();
 
   createButtons();
 
   randomSlider = createSlider(1, 1000, 500);
-  randomSlider.parent('controls');
-  randomSlider.html("test");
+  randomSlider.parent('sliders');
+  randomSlider.html('randomness');
+
+  sizeSlider = createSlider(1, 50, 1)
+  sizeSlider.parent('sliders');
+  sizeSlider.html('size');
   
+  noStroke();
   // canvas.position(100,100);
 }
 
 function draw() {
   changeMode();
   randomness = randomSlider.value()/100;
+  bugSize = sizeSlider.value();
 
+  // Just for the shape of the 
   switch(thingType) {
     case 'bug':
-      fill(250,250,250);
-      ellipse(mouseX, mouseY, 15, 15);
+      fill(250,250,250, 150);
+      ellipse(mouseX, mouseY, 8, 8);
       break;
     case 'anchor':
       fill(0,0,0);
-      ellipse(mouseX, mouseY, 40, 40);
+      ellipse(mouseX, mouseY, 25, 25);
       break;
     case 'eraser':
       fill(255,255,255);
@@ -52,27 +62,20 @@ function draw() {
       rect(mouseX, mouseY, 40, 40);
       break;
     case 'repulsor':
-      fill(255, 255, 255, 150);
+      fill(255, 255, 255, 20);
+      stroke(255, 255, 255, 150)
       ellipse(mouseX, mouseY, 60, 60);
+      noStroke();
       break;
   }
 
+  // Bug creation is here, but anchor creation is in mouseClicked function to prevent multiples
   if (mouseIsPressed && onCanvas === true) {
     if (thingType === 'bug') {
       bugList.push(new Jitter());
-    } else if (thingType === 'anchor') {
-      anchorList.push(new Anchor());
     } 
   }
   
-  for (i = 0; i < bugList.length; i++) {
-    bug = bugList[i];
-    bug.move();
-    bug.display();
-    if  (thingType === 'eraser' && mouseIsPressed && dist(bug.x, bug.y, mouseX, mouseY) < 20){
-      bugList.splice(i, 1);
-    }
-  }
   for (i = 0; i < anchorList.length; i++) {
     anchor = anchorList[i];
     anchor.display();
@@ -80,7 +83,17 @@ function draw() {
       anchorList.splice(i, 1);
     }
   }
+
+    for (i = 0; i < bugList.length; i++) {
+    bug = bugList[i];
+    bug.move();
+    bug.display();
+    if  (thingType === 'eraser' && mouseIsPressed && dist(bug.x, bug.y, mouseX, mouseY) < 20){
+      bugList.splice(i, 1);
+    }
+  }
 }
+
 
 function makeButtons (text, method) {
   button = createButton(text);
@@ -92,18 +105,25 @@ function makeButtons (text, method) {
 
 function createButtons() {
   
-  makeButtons('Bugs', function() { thingType = 'bug'; });
+  makeButtons('Bug', function() { thingType = 'bug'; });
   makeButtons('Anchor', function() { thingType = 'anchor'; });
   makeButtons('Eraser', function() { thingType = 'eraser'; });
-  makeButtons('Repulse', function() {thingType = 'repulsor'});
-  makeButtons('Reset', eraseEverything);
+  makeButtons('Pusher', function() {thingType = 'repulsor'});
   makeButtons('Switch Mode', function() { modeCurrent++; })
-
+  makeButtons('Reset', eraseEverything);
 
 };
 
 function windowResized() {
   resizeCanvas(windowWidth-100, windowHeight-100);
+}
+
+
+function mouseClicked() {
+  // Put anchor in this function so that multiples wouldn't be created
+  if (thingType === 'anchor' && onCanvas === true) {
+      anchorList.push(new Anchor());
+  }
 }
 
 function changeMode() {
@@ -112,20 +132,25 @@ function changeMode() {
     firstTimeSwitch = true;
   } else if (modeCurrent === 0) {
     background(40, 40, 40);
-    bugColor = [255,255,255,255]
+    bugOutline = [0, 0, 0, 0]
+    bugColor = [255,255,255,230]
   } else if (modeCurrent === 1) {
     fill(255, 10); // semi-transparent white
+    bugOutline = [0, 0, 0, 255]
     rect(-1, -1, width+2, height+2);
   } else if (modeCurrent === 2) {
     if (firstTimeSwitch === true) {
       background(255,255,255);
+      bugOutline = [0, 0, 255, 200]
       firstTimeSwitch = false;
     }
+
     noStroke();
     fill(255, 3); 
     rect(0, 0, width, height);
 
     bugColor = [0,100,255,150]; 
+
   }
 };
 
@@ -138,7 +163,7 @@ function eraseEverything() {
 function Anchor() {
   this.x = mouseX;
   this.y = mouseY;
-  this.diameter = 50;
+  this.diameter = 10;
   var anchorRedFill = 154;
   var goUp = true;
 
@@ -157,7 +182,7 @@ function Anchor() {
     }
 
     fill(anchorRedFill, 0, 0);
-    this.diameter = (anchorRedFill/20)+40;
+    this.diameter = (anchorRedFill/20)+25;
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
@@ -167,7 +192,7 @@ function Anchor() {
 function Jitter() {
   this.x = mouseX;
   this.y = mouseY;
-  this.diameter = random(5, 15);
+  this.diameter = random(2, 5);
   var attracted = false;
 
   this.move = function() {
@@ -215,8 +240,10 @@ function Jitter() {
 
   this.display = function() {
     // this.diameter = this.diameter + random(-randomness/100, randomness/100);
+    stroke(bugOutline);
+    currentDiameter = this.diameter * bugSize;
     fill(bugColor);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+    ellipse(this.x, this.y, currentDiameter, currentDiameter);
 
   }
 };
